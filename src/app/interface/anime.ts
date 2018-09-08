552bbaa978fc818586336edbcdfe89f6936c5821
otera <capacityoverflow@reminiscence.sakura.ne.jp>
2018-09-08 11:48:12 +0900

    続編ものかどうかわかるようカラムを追加

--- src/app/interface/anime.ts
+++ src/app/interface/anime.ts
@@ -3,6 +3,7 @@ export interface Anime {
     title:string; //作品タイトル
     title_short1:string; //略称
     public_url:string; //公式サイト
-    sex:number;//0:男性向け 1:女性向け
+    sex:number; //0:男性向け 1:女性向け
     twitter_account:string; //Twitterアカウント
+    sequel:number; //1以上:続編
 }
