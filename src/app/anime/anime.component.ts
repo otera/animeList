f77775f3340320974fd2d18f34eab66c63728896
otera <capacityoverflow@reminiscence.sakura.ne.jp>
2020-06-28 15:19:00 +0900

    css to scss

--- src/app/anime/anime.component.ts
+++ src/app/anime/anime.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 import { ApiService } from "../service/api.service";
 import { Anime } from "../interface/anime";
 import { Years } from "../interface/years";
 
-import * as moment from 'moment';
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-anime',
-  templateUrl: './anime.component.html',
-  styleUrls: ['./anime.component.css']
+  selector: "app-anime",
+  templateUrl: "./anime.component.html",
+  styleUrls: ["./anime.component.scss"],
 })
 export class AnimeComponent implements OnInit {
-
   // セレクトボックスの中身を生成するためのデータ
   private seasons = [
     { season: "(冬)", phase: "1" },
@@ -31,26 +30,34 @@ export class AnimeComponent implements OnInit {
   selYear: number;
 
   // テーブル項目
-  displayedColumns: string[] = ['title', 'title_short1', 'sequel', 'target', 'twitter'];
+  displayedColumns: string[] = [
+    "title",
+    "title_short1",
+    "sequel",
+    "target",
+    "twitter",
+  ];
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {}
 
   // セレクトボックスの中身を生成
   createSelOption() {
-    const MAX_YYYY = Number(moment().format('YYYY')); // 今年
+    const MAX_YYYY = Number(moment().format("YYYY")); // 今年
     let yyyy = 2014; // API対応開始年
 
     for (; yyyy <= MAX_YYYY; yyyy++) {
-      this.seasons.forEach(season => {
+      this.seasons.forEach((season) => {
         this.years.push({
-          year: yyyy, display: yyyy + "年" + season.season, phase: season.phase
+          year: yyyy,
+          display: yyyy + "年" + season.season,
+          phase: season.phase,
         });
       });
     }
 
     // 新しい順にする
     this.years.sort((a, b) => {
-      if(a.year > b.year){
+      if (a.year > b.year) {
         return -1;
       }
       if (a.year < b.year) {
@@ -62,24 +69,23 @@ export class AnimeComponent implements OnInit {
       // a.phase < b.phase
       return 1;
     });
-  };
+  }
 
   ngOnInit() {
     this.createSelOption();
   }
 
   // serviceにリクエスト要求
-  requestSend(params: { [key: string]: string; }) {
-    this.apiService.request("master/" + params.year + "/" + params.phase).subscribe(
-      data => {
+  requestSend(params: { [key: string]: string }) {
+    this.apiService
+      .request("master/" + params.year + "/" + params.phase)
+      .subscribe((data) => {
         this.animedata = data;
-      }
-    );
+      });
   }
 
   // セレクトボックス変更イベント
   onChangeYear(event) {
     this.requestSend(event.value);
   }
-
 }
